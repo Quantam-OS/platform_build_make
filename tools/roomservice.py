ccf11dbe0a38507ec4026acee50daa41d685592d
mdeejay <mdjrussia@gmail.com>
2019-05-11 15:05:04 +0200

    Fix roomservice

--- tools/roomservice.py
+++ tools/roomservice.py
@@ -46,8 +46,8 @@ default_manifest = ".repo/manifest.xml"
 custom_local_manifest = ".repo/local_manifests/beast_manifest.xml"
 custom_default_revision = "pie"
 custom_dependencies = "beast.dependencies"
-org_manifest = "Beast-devices"  # leave empty if org is provided in manifest
-org_display = "Beast-Devices"  # needed for displaying
+org_manifest = "BeastRoms-Devices"  # leave empty if org is provided in manifest
+org_display = "BeastRoms-Devices"  # needed for displaying
 
 github_auth = None
 
